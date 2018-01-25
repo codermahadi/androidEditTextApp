c5be7d1c6ac8374f7898d73a6b6a2fd5cb6b08e3
codermahadi <31430772+codermahadi@users.noreply.github.com>
2018-01-26 00:32:52 +0600

    working with MainActivity.java making simple calculator using

--- app/src/main/java/com/example/mahadi/edittextapp/MainActivity.java
+++ app/src/main/java/com/example/mahadi/edittextapp/MainActivity.java
@@ -2,12 +2,62 @@ package com.example.mahadi.edittextapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText number1,number2;
+    private Button btn_add,btn_subs;
+    private TextView resultShow;
+    double result;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        number1 = (EditText) findViewById(R.id.edit_text);
+        number2 = (EditText) findViewById(R.id.edit_text2);
+
+        btn_add  = (Button) findViewById(R.id.add);
+        btn_subs = (Button) findViewById(R.id.subs);
+
+        resultShow = (TextView) findViewById(R.id.resultShow);
+
+        btn_add.setOnClickListener(this);
+        btn_subs.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        String num1 = number1.getText().toString();
+        String num2 = number2.getText().toString();
+
+        //Converting into double
+
+        double n1 = Double.parseDouble(num1);
+        double n2 = Double.parseDouble(num2);
+
+        switch (view.getId()){
+
+            case R.id.add:
+                result = n1+n2;
+                resultShow.setText(n1 + " + " + n2 + " = " + result);
+                break;
+
+            case R.id.subs:
+                result = n1-n2;
+                resultShow.setText(n1 + " - " + n2 + " = " + result);
+                break;
+
+        }
     }
 }
